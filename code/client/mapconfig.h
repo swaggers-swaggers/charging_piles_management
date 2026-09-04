87e3455a33aa481914f25a3156429a0d6214cd24
高若涵 <2454685816@qq.com>
2026-09-04 16:27:37 +0800

    key

---
+++ code/client/mapconfig.h
@@ -0,0 +1,8 @@
+// 你的真实 Key 配置(此文件已被 .gitignore 忽略, 不会提交到 GitHub)
+// 请把下面 "YOUR_KEY" 替换成你申请的腾讯地图 Key。
+#ifndef MAPCONFIG_H
+#define MAPCONFIG_H
+
+#define TENCENT_MAP_KEY "YYMBZ-PWO6L-OWMPR-E7TOY-PTK7H-2EFEG"
+
+#endif // MAPCONFIG_H
